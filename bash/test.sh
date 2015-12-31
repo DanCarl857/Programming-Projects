05c80fcbfcb2caa25eec729a4351efd59798722a
DanCarl857 <dancarl758@gmail.com>
2015-12-31 11:43:49 +0100

    Trying out shell scripting

---
+++ bash/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a basic bash script
+
+today=$(date +"%d-%m-%Y")
+time=$(date +"%H:%M:%S")
+printf -v d "Current User:\t%s\nDate:\t\t%s @ %s\n" $USER $today $time
+echo "$d"
