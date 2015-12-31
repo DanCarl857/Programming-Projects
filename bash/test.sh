ed724729a47afd36f4c7554b0ad8378e0636de6e
DanCarl857 <dancarl758@gmail.com>
2015-12-31 11:48:59 +0100

    arrays in bash

--- bash/test.sh
+++ bash/test.sh
@@ -6,3 +6,16 @@ today=$(date +"%d-%m-%Y")
 time=$(date +"%H:%M:%S")
 printf -v d "Current User:\t%s\nDate:\t\t%s @ %s\n" $USER $today $time
 echo "$d"
+
+echo
+echo
+echo
+a=()
+b=("apple" "mango" "cherry")
+echo ${b[2]}
+echo 
+echo
+b[5]="kiwi"
+b+=("mango")	# adds to the end of the array
+echo ${b[@]}	# prints out the whole array
+echo ${b[@]: -1}	# get the last element
