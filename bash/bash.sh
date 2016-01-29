ca35645a6ab818a2aefbd1593aa242df3fced0cf
DanCarl857 <dancarl758@gmail.com>
2016-01-29 22:05:22 +0100

    Mozilla stuff

--- bash/bash.sh
+++ bash/bash.sh
@@ -8,8 +8,7 @@ echo
 echo "Positional parameter 1 for the script is $1."
 echo
 
-test ()
-{
+test () {
 	echo "Positional parameter 1 in the function is $1."
 	RETURN_VALUE=$?
 	echo "The exit code of this function is $RETURN_VALUE."
